f072622ffa81bb0be878731ae5c29fd4c53f4631
Leonardo <leonardo_soares@live.com>
2019-10-30 16:07:13 -0300

    Finaliza Forms

--- view/frmCliente.py
+++ view/frmCliente.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 class Application:
     def __init__(self, master=None):
         self.fontePadrao = ("Arial", "10")
@@ -61,16 +62,58 @@ class Application:
         self.email["width"] = 20
         self.email.pack( side=LEFT )
 
-        self.tipo = Label( self.container5, text="Tipo: ", font=self.fontePadrao )
-        self.tipo.pack( side=LEFT )
-        self.tipo = Entry( self.container5 )
-        self.tipo["width"] = 20
-        self.tipo.pack( side=LEFT )
+        #self.tipo = Label( self.container5, text="Tipo: ", font=self.fontePadrao )
+        #self.tipo["width"] = 20
+        #self.tipo.pack(side=LEFT)
+        self.var = StringVar
+        self.tipoPF = Radiobutton(self.container5, text="Pessoa Física",  value="PF", variable=self.var)
+        self.tipoPJ = Radiobutton(self.container5, text="Pessoa Jurídica", value="PJ", variable=self.var)
+        self.tipoPF.pack()
+        self.tipoPJ.pack()
+
+        self.cpf = Label( self.container6, text="CPF: ", font=self.fontePadrao )
+        self.cpf.pack( side=LEFT )
+        self.cpf = Entry( self.container6 )
+        self.cpf["width"] = 20
+        self.cpf.pack( side=LEFT )
+
+        self.cnpj = Label( self.container7, text="CNPJ: ", font=self.fontePadrao )
+        self.cnpj.pack( side=LEFT )
+        self.cnpj = Entry( self.container7 )
+        self.cnpj["width"] = 20
+        self.cnpj.pack( side=LEFT )
+
+        self.cidade = Label( self.container8, text="Cidade: ", font=self.fontePadrao )
+        self.cidade.pack( side=LEFT )
+        self.cidade = ttk.Combobox(self.container8, values=self.GetCidades())
+        self.cidade["width"] = 20
+        self.cidade.pack( side=LEFT )
+
+        self.receberemail = Checkbutton(self.container9, text="Aceiro receber e-mail", font=self.fontePadrao)
+        self.receberemail["width"] = 20
+        self.receberemail.pack( side=LEFT )
+
+        self.save = Button( self.container10, text="Salvar", font=self.fontePadrao )
+        self.save.pack( side=LEFT )
+        self.reset = Button( self.container10, text="Limpar", font=self.fontePadrao )
+        self.reset.pack( side=RIGHT )
 
     def geraCodigoCliente(self):
         return "0000"
 
+    def GetCidades(self):
+        return ["", "Porto Alegre", "Cachoeirinha", "Canoas", "Gravatai"]
+
+    def rButt(self):
+        PfPj = [("Pessoa Física", "PF"), ("Pessoa Jurídica", "PJ")]
+        v = StringVar()
+        v.set("L")
+        for t, md in PfPj:
+            b = Radiobutton(text=t, variable=v, value=md)
+            b.pack(anchor=W)
+
+
 root = Tk()
 Application(root)
-root.geometry( '300x300' )
+root.geometry( '300x450' )
 root.mainloop()
